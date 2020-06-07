86ce42975567850949a5e6e5646bd36ee409dff9
grmaier <64373365+grmaier@users.noreply.github.com>
2020-06-07 21:35:04 +0200

    correct bit shifting

--- child_id.c
+++ child_id.c
@@ -1,12 +1,10 @@
 #include "tree_based_quadrants.h"
-#include "math.h"
 
 int child_id (const quadrant_t *q) {
-	int h = pow(2, QUADRANT_LEN(q->level));
 	int res = 0;
-	res += (q->x & h ? 1 : 0);
-	res += 2*(q->y & h ? 1 : 0);
-	res += 4*(q->z & h ? 1 : 0);
+	res += (q->x & QUADRANT_LEN(q->level) ? 1 : 0);
+	res += ((q->y & QUADRANT_LEN(q->level) ? 1 : 0) << 1);
+	res += ((q->z & QUADRANT_LEN(q->level) ? 1 : 0) << 2);
 	
 	return res;
 }
